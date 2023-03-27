3920b96d8d9f42e495bd1df0e9de77388104fb53
Ar12agnik <102238499+Ar12agnik@users.noreply.github.com>
2023-03-27 21:15:44 +0530

    Update interpolationsearch.py

--- data structures and algorithm/interpolationsearch.py
+++ data structures and algorithm/interpolationsearch.py
@@ -7,22 +7,14 @@ search=int(input("enter an element to be searched: "))
 found=0
 while (low<=high):
     pos=round(low+((search-x[low])/(x[high]-x[low]))*(high-low))
-    
-    
-
     if array[pos]==search:
         found=1
         print(f"Element found at {pos}")
         break
     elif array[pos]>search:
-        high=pos-1
-        
+        high=pos-1        
     elif array[pos]<search:
-        low=pos+1
-        
-        
-        
-    
+        low=pos+1    
 if found==0:
     print("element not found!")
 
